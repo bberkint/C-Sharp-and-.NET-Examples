001490ebb334db824d3541ebdd089c1ea343261d
BBerkinT <topaloglubatinberkin@gmail.com>
2023-08-09 17:12:18 +0300

    DataAccessLayer'a gerekli paketleri yükledim

--- CoreDemo/EntityLayer/Concrete/Comment.cs
+++ CoreDemo/EntityLayer/Concrete/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace EntityLayer.Concrete
 {
     public class Comment
     {
+        [Key]
         public int CommentID { get; set; }
         public string CommentUserName { get; set; }
         public string CommentTitle { get; set; }
--- CoreDemo/EntityLayer/Concrete/Contact.cs
+++ CoreDemo/EntityLayer/Concrete/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace EntityLayer.Concrete
 {
     public class Contact
     {
+        [Key]
         public int ContactID { get; set; }
         public string ContactUserName { get; set; }
         public string ContactMail { get; set; }
