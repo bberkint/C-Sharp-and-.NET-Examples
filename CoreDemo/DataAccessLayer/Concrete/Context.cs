d06647722fa1861e9ee21a8a736b10b779d331dd
BBerkinT <topaloglubatinberkin@gmail.com>
2023-08-17 13:05:07 +0300

    .

--- CoreDemo/DataAccessLayer/Concrete/Context.cs
+++ CoreDemo/DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-    internal class Context
+    public class Context : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("server = BERKINPC\\SQLEXPRESS;database = CoreBlogDb: integrated security = true;");
+        }
+
+        public DbSet<About> AboutS { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Context> Contexts { get; set; }
+
+        public DbSet<Writer> Writers { get; set; }
+
     }
+
+
 }
